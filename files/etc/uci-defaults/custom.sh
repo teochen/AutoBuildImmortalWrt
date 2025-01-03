4c736ac738ad71e83afe0742045c9e98fd57dd1f
teochen <youyoubeibei@gmail.com>
2025-01-04 00:10:46 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -33,9 +33,15 @@ uci delete ttyd.@ttyd[0].interface
 uci set dropbear.@dropbear[0].Interface=''
 uci commit
 
+# 配置 PPPoE 拨号
+uci set network.wan.proto='pppoe'
+uci set network.wan.username='031006204681@adsl'
+uci set network.wan.password='123456'
+uci commit network
+
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by wukongdaily"
+NEW_DESCRIPTION="Compiled by teochen"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
